03dea63f062bda1c3c85e3e68c10c709b64a3239
vanshika0227 <vanshika.0227@gmail.com>
2023-04-15 22:15:08 +0530

    server side changes

--- swift-bill-server/server.js
+++ swift-bill-server/server.js
@@ -1,4 +1,5 @@
 import readXlsxFile from 'read-excel-file/node';
+import writeXlsxFile from 'write-excel-file/node';
 import path from 'path';
 import express from 'express';
 import cors from 'cors';
@@ -13,6 +14,57 @@ let filename = path.join(process.cwd(), 'dataBase', 'varis.xlsx');
 app.get('/client', async (req, res) => {
     let content = await readXlsxFile(filename)
     res.send(content);
+});
+
+app.post('/client', async (req, res) => {
+  let content = await readXlsxFile(filename)
+  let sheetData = []
+  content.forEach(row => {
+    let rowData = []
+    row.forEach((cell) => {
+      rowData.push({
+          value: cell.toString(),
+          type: String
+        });
+    });
+    sheetData.push(rowData);
+  });
+  console.log(req.body);
+  console.log(JSON.stringify(req.body));
+  sheetData.push([
+    {
+      value: req.body.clientName.toString(),
+      type: String
+    },
+    {
+      value: req.body.GST_number.toString(),
+      type: String
+    },
+    {
+      value: req.body.PlaceOfSupply.toString(),
+      type: String
+    },
+    {
+      value: req.body.GST_Type.toString(),
+      type: String
+    },
+    {
+      value: req.body.Billing_Address.toString(),
+      type: String
+    },
+    {
+      value: req.body.Shipping_Address.toString(),
+      type: String
+    }
+  ])
+  try {
+    await writeXlsxFile(sheetData, {filePath: filename})
+    res.status(200).send()
+  } catch(err){
+    console.log(err);
+    res.status(400).send(err);
+  }
+  
 });
 
 app.get('/bills', (req, res) => {
