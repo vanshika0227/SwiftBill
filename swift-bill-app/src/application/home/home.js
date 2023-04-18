0d2178ab385aa1b3586ce006759e4844a57bc876
vanshika0227 <vanshika.0227@gmail.com>
2023-04-18 17:26:58 +0530

    footer fix

--- swift-bill-app/src/application/home/home.js
+++ swift-bill-app/src/application/home/home.js
@@ -6,6 +6,7 @@ import "@fontsource/roboto"
 import { Grid, Container } from "@mui/material"
 import { Outlet, useLocation} from "react-router-dom"
 import ErrorBoundary from "../errorBoundary/ErrorBoundary"
+import Paper from "@mui/material/Paper"
 import "./Home.css"
 
 const Home = () => {
@@ -17,15 +18,17 @@ const Home = () => {
             <Header />
         </Grid>
         <Grid item lg={2} md={2} sm={2} xs={2}>
-            <SideNavbar></SideNavbar>
+            <SideNavbar/>
         </Grid>
         <Grid item lg={10} md={10} sm={10} xs={10}>
             <ErrorBoundary key={useLocation().pathname}>
-            <Outlet />
+                <Outlet />
             </ErrorBoundary>
         </Grid>
         <Grid item lg={12} md={12} sm={12}>
-            <Footer />
+            <Paper sx={{ position: 'relative', bottom: 0, left: 0, right: 0 }}>
+                <Footer />
+            </Paper>
         </Grid>
         </Grid>
     </Container>
