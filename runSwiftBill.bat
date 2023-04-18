0175ce26e14a9e6fb1cd7e920d219c8f5df27559
vanshika0227 <vanshika.0227@gmail.com>
2023-04-18 22:17:41 +0530

    batch file to run application

--- runSwiftBill.bat
+++ runSwiftBill.bat
@@ -1,12 +1,9 @@
 echo "Running swift bill application"
 
-(
 cd "swift-bill-app"
-START "" /WAIT "npm install --frozen-lock"
+start  /B /wait cmd /c "npm install --frozen-lock"
 cd "..\swift-bill-server"
-START ""/WAIT "npm install --frozen-lock"
-cd .".."
-start "" /D "swift-bill-app" "npm start"
-start "" /D "swift-bill-server" "npm start"
-) | pause
-Echo waited
+start /B /wait cmd /c "npm install --frozen-lock"
+cd ".."
+start /B /D "swift-bill-app" cmd /c "npm start"
+start /B /D "swift-bill-server" cmd /c "npm start"
