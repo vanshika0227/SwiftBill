a8163aa8db1728850f135ec40c3b91480048c06d
vanshika0227 <vanshika.0227@gmail.com>
2023-05-03 18:02:49 +0530

    enter URP in case GST is not applicable

--- swift-bill-app/src/application/invoice/new-invoice.js
+++ swift-bill-app/src/application/invoice/new-invoice.js
@@ -207,7 +207,7 @@ const New = () => {
   };
 
   const validateInputs = (value) => {
-    setError((error) => ({ ...error, gstError: (value.GST_Number.length !== 15 || !/^[A-Za-z0-9]*$/.test(value.GST_Number))}))
+    setError((error) => ({ ...error, gstError: ((value.GST_Number.length !== 15 || value.GST_Number !== 'URP') && !/^[A-Za-z0-9]*$/.test(value.GST_Number))}))
     setError((error) => ({ ...error, priceError: isNumberUpto2Decimal(value.Price)}))
     setError((error) => ({ ...error, quantityError: isNumberUpto2Decimal(value.Quantity)}))
   }
